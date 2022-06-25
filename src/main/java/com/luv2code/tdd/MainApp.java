7fc2f6cc63aa0de692cac689858f8790ff6deba5
xiangchen09 <107827922+xiangchen09@users.noreply.github.com>
2022-06-24 20:26:01 -0400

    commit stashed code #2 (#3)

--- src/main/java/com/luv2code/tdd/MainApp.java
+++ src/main/java/com/luv2code/tdd/MainApp.java
@@ -4,6 +4,14 @@ public class MainApp {
     public static void main(String[] args) {
         for (int i = 1; i <= 100 ; i++) {
             System.out.println(FizzBuzz2.compute(i));
+
+            //add on stash branch
+            System.out.println("1");
+            System.out.println("2");
+
+            //add another line of code with stash#2
+            System.out.println("3");
+            System.out.println("4");
         }
     }
 
